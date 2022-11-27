956315f47e5e3dc8d8172f45398fca0a293b42f5
Amalia Capatina <92114298+amalia6@users.noreply.github.com>
2022-11-27 21:44:13 +0200

    Add project files.

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(791, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(747, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Click for opening file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(747, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Insert data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(747, 307);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(936, 659);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Graphics graph;
+        StreamWriter write = new StreamWriter("punctuletze.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double Gauss(double x, double m, double dispersia)
+        {
+            double var1 = Math.Pow((m - x), 2.0);
+            double var2 = var1 / (2.0 * Math.Pow(dispersia, 2));
+            double var3 = Math.Exp(-var2);
+
+            return var3;
+        }
+
+        public void DrawAxis(Point origin)
+        {
+            graph = panel1.CreateGraphics();
+            Pen myPen = new Pen(Color.Black);
+
+            Point p1 = new Point(origin.X - 300, origin.Y);
+            Point p2 = new Point(origin.X + 300, origin.Y);
+            Point p3 = new Point(origin.X, origin.Y-300);
+            Point p4 = new Point(origin.X, origin.Y+300);
+
+            graph.TranslateTransform(300, 300);
+            graph.DrawLine(myPen, p1, p2);
+            graph.DrawLine(myPen, p3, p4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = @"D:\Laptop Amalia\Facultate\anul IV\invatare automata\lab1\lab1\bin\Debug\punctuletze.txt";
+            Process.Start(file);
+        }
+
+        public void DesenareZona(Pen myPen,int mx, int my, int sigmax, int sigmay)
+        {
+            graph = panel1.CreateGraphics();
+
+            Point stangaSus = new Point(300 + (mx - sigmax), 300-(my + sigmay));
+            Point stangaJos = new Point(300+(mx - sigmax), 300-(my - sigmay));
+            Point dreaptaSus = new Point(300+(mx + sigmax), 300-(my + sigmay));
+            Point dreaptaJos = new Point(300+(mx + sigmax), 300-(my - sigmay));
+
+            graph.DrawLine(myPen, stangaSus, stangaJos);
+            graph.DrawLine(myPen, stangaJos, dreaptaJos);
+            graph.DrawLine(myPen,dreaptaJos, dreaptaSus);
+            graph.DrawLine(myPen, dreaptaSus, stangaSus);
+        }
+
+        public void GenerarePuncte(Pen pen,int nrPuncte, double mx, double sigmax, double my, double sigmay) 
+        {
+            Random rand = new Random();
+            int coordX, coordY;
+            int contor = 0;
+
+            while (contor < nrPuncte)
+            {
+            Start1:
+                int x = rand.Next(-300, 300);
+                double probabilitatex = Gauss(x, mx, sigmax);
+                double pax = rand.Next(0, 1001) / 1000.0;
+
+                if (probabilitatex >= pax)
+                {
+                    coordX = x;
+                }
+                else
+                {
+                    goto Start1;
+                }
+            Start2:
+                int y = rand.Next(-300, 300);
+                double probabilitatey = Gauss(y, my, sigmay);
+                double pay = rand.Next(0, 1001) / 1000.0;
+
+                if (probabilitatey >= pay)
+                {
+                    coordY = y;
+                }
+                else
+                {
+                    goto Start2;
+                }
+                contor++;
+
+                graph.DrawRectangle(pen, 300 + coordX, 300 - coordY, 1, 1);
+                write.WriteLine("coord_X: " + coordX + ", coord_Y: " + coordY );
+
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen red = new Pen(Color.Red);
+            Pen yellow = new Pen(Color.Yellow);
+            Pen blue = new Pen(Color.Blue);
+
+            Point origin = new Point(0, 0);
+            DrawAxis(origin);
+
+
+            DesenareZona(red, 180, 220, 20, 20);
+            GenerarePuncte(red, 1500, 180, 10, 220, 10);
+
+            DesenareZona(blue,-100, 110, 30, 20);
+            GenerarePuncte(blue, 1500, -100, 15, 110, 10);
+
+            DesenareZona(yellow,210, -150, 10, 40);
+            GenerarePuncte(yellow,500, 210, 5, -150, 20);
+
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = String.Format("{0},{1}", e.Location.X, e.Location.Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pen red = new Pen(Color.Red);
+
+            textBox1.Location = new Point(747, 307);
+            textBox1.Visible = true;
+
+            //int x = int.Parse(textBox1.Text);
+        }
+    }
+}
